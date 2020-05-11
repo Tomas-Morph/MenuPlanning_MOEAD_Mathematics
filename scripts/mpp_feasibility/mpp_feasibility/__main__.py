aa0f52839c69be3d3c31f669db5f8adf4de345af
amarrerod <alemarrerodiaz@gmail.com>
2020-05-11 13:09:55 +0100

    Script working
    q

--- scripts/mpp_feasibility/mpp_feasibility/__main__.py
+++ scripts/mpp_feasibility/mpp_feasibility/__main__.py
@@ -4,9 +4,25 @@
     ---
     Nutrients Requirements
 """
+import argparse
+from mpp_feasibility.mpp_data import compute_requirements, read_database
+from mpp_feasibility.feasibility_calcultator import compute_feasibility
+from mpp_feasibility.result_files_parser import file_list, parse_all_files
 
-from mpp_feasibility.nutrients_req import compute_requirements
-from mpp_feasibility.menu_parser import create_datasets
+parser = argparse.ArgumentParser('Menu Planning Problem Feasibility Checker')
+parser.add_argument('path_to_files')
+parser.add_argument('num_of_days', type=int)
+parser.add_argument('output_filename')
+parser.add_argument('-v', '--verbose', action='store_true')
+args = parser.parse_args()
 
-recomendations = compute_requirements(10, verbose=True)
-main_courses_df, second_courses_df, desserts_df = create_datasets(verbose=True)
+requirements = compute_requirements(args.num_of_days, args.verbose)
+main_df, second_df, dess_df = read_database(args.verbose)
+files_to_parse = file_list(args.path_to_files)
+results = parse_all_files(files_to_parse)
+
+for config in results:
+    print(f'Configuration: {config}')
+    for individual in results[config]:
+        compute_feasibility(individual, requirements, args.num_of_days,
+                            main_df, second_df, dess_df, args.verbose)
